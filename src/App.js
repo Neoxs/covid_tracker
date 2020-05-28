b04c4c70bb9c73bea2d8bf7358987ed590af1dbf
Neoxs <m.abdelkaderkharoubi@esi-sba.dz>
2020-05-28 02:08:27 +0100

    adding react carts

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import ReactDOM from 'react-dom'
 
 import { Cards, Chart, CountryPicker } from './components'
 import styles from './App.module.css'
+import { fetchData } from './api'
 
 class App extends Component {
+
+    state= {
+        data: {},
+    }
+
+    async componentDidMount() {
+        const data = await fetchData()
+
+        this.setState({ data })
+    }
+
     render () {
+        const { data } = this.state
+
         return (
             <div className={styles.container}>
-                <Cards />
+                <Cards data={data} />
                 <CountryPicker />
                 <Chart />
             </div>
